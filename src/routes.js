3f1dcd762e7ef29382c639eb75761269947c0019
KelvinLopes <kelvin7android@gmail.com>
2020-01-20 22:49:02 -0300

    Updata para Routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
-const DevCoordinatesController = require('./controllers/DevCoordinatesController');
 const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
@@ -9,7 +8,6 @@ routes.get('/devs', DevController.index);
 
 routes.post('/devs', DevController.store);
 routes.put('/devs/:_id', DevController.update);
-routes.put('/devs/coordinates/:_id', DevCoordinatesController.update);
 
 routes.delete('/devs/:_id', DevController.delete);
 
